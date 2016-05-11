586b043caa196e6d7a4c68a4a79cad1ff953e280
czross <czr@uw.edu>
2016-05-11 13:07:01 -0700

    final version I be done now

--- js/main.js
+++ js/main.js
@@ -3,19 +3,7 @@
  */
 
 $(function() {
-
-
-   d3.json("data.json", function(error, root) {
-       if(error) throw error;
-
-       var chart = TreeChart().height(2200);
-
-       chart.nodeColor("blue");
-       chart.linkColor("red");
-       var div = d3.select("#my-Div").datum(root).call(chart);
-
-
-    });
+    //Your code to make the chart goes here. 
 
 
 });
